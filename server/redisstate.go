8f933d1b8e952756a7f204d8ee3616558542af5b
Chris Hager <chris@linuxuser.at>
2021-12-14 21:43:51 +0100

    isWhitehatBundleCollection, redis:ltrim

--- server/redisstate.go
+++ server/redisstate.go
@@ -34,8 +34,8 @@ var RedisPrefixSenderMaxNonce = RedisPrefix + "txsender-pending-max-nonce:"
 var RedisExpirySenderMaxNonce = time.Duration(2 * time.Hour)
 
 // Enable lookup of bundle txs by bundleId
-var RedisPrefixBundleTransactions = RedisPrefix + "cached-tx-for-bundle-id:"
-var RedisExpiryBundleTransactions = time.Duration(24 * time.Hour) // 1 day
+var RedisPrefixWhitehatBundleTransactions = RedisPrefix + "tx-for-whitehat-bundle:"
+var RedisExpiryWhitehatBundleTransactions = time.Duration(24 * time.Hour) // 1 day
 
 // // Enable lookup of last privateTransaction-txHash sent by txFrom
 // var RedisPrefixLastPrivTxHashOfAccount = RedisPrefix + "last-txhash-of-txsender:"
@@ -61,8 +61,8 @@ func RedisKeySenderMaxNonce(txFrom string) string {
 	return RedisPrefixSenderMaxNonce + strings.ToLower(txFrom)
 }
 
-func RedisKeyBundleTransactions(bundleId string) string {
-	return RedisPrefixBundleTransactions + strings.ToLower(bundleId)
+func RedisKeyWhitehatBundleTransactions(bundleId string) string {
+	return RedisPrefixWhitehatBundleTransactions + strings.ToLower(bundleId)
 }
 
 // func RedisKeyLastPrivTxHashOfAccount(txFrom string) string {
@@ -191,9 +191,15 @@ func (s *RedisState) GetSenderOfTxHash(txHash string) (txSender string, found bo
 //
 // Enable lookup of tx bundles by bundle ID
 //
-func (s *RedisState) AddTxToBundle(bundleId string, signedTx string) error {
-	key := RedisKeyBundleTransactions(bundleId)
-	err := s.RedisClient.LPush(context.Background(), key, signedTx, RedisExpiryBundleTransactions).Err()
+func (s *RedisState) AddTxToWhitehatBundle(bundleId string, signedTx string) error {
+	key := RedisKeyWhitehatBundleTransactions(bundleId)
+	err := s.RedisClient.LPush(context.Background(), key, signedTx, RedisExpiryWhitehatBundleTransactions).Err()
+	if err != nil {
+		return err
+	}
+
+	// Limit to 15 entries
+	err = s.RedisClient.LTrim(context.Background(), key, 0, 15).Err()
 	return err
 }
 
